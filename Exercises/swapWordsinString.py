a4eacd3675c8c1c4d5a313b8e170efe9b52be125
Your Name <yraghu13@gmail.com>
2020-10-21 12:26:14 +0200

    used swapcase functionality

---
+++ Exercises/swapWordsinString.py
@@ -0,0 +1,11 @@
+name = "RaGhU vArMa YaRAkaRaJu"
+new = name.split()  # split converts string into list of strings
+
+new.reverse()
+print(new)
+
+mix = ' '.join(new)     # join converts List into string
+
+a = mix.swapcase()   # swapcase converts lowercase to uppercase and viceversa
+
+print(a)
