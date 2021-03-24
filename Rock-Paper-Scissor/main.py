ad353dd91e86d0c7d15b5d7cd963fe7bc928aa7a
raghu826 <39645062+raghu826@users.noreply.github.com>
2021-03-24 22:01:19 +0100

    rockPaperScissor  implemented

---
+++ Rock-Paper-Scissor/main.py
@@ -0,0 +1,28 @@
+from random import randint
+
+choice = ['rock', 'paper', 'scissor']
+
+for i in range(50):
+    computer = choice[randint(0, 2)]
+    select = input("rock or paper or scissor?")
+    if select == computer:
+        print("tie")
+    else:
+        if select == "rock":
+            if computer == "paper":
+                print("You lose!", computer, "covers", select)
+            else:
+                print("You win!", select, "smashes", computer)
+        elif select == "paper":
+            if computer == "scissor":
+                print("You lose!", computer, "cut", select)
+            else:
+                print("You win!", select, "covers", computer)
+        elif select == "scissor":
+            if computer == "rock":
+                print("You lose...", computer, "smashes", select)
+            else:
+                print("You win!", select, "cut", computer)
+    if select not in choice:
+        print("check spelling")
+
